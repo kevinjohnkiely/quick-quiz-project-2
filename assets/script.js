abf948b253a1124b22911489124e5f7f50d02241
Kevin John Kiely <kevinjohnkiely1@hotmail.com>
2022-04-13 13:32:57 +0000

    Add global variables to script file

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,13 @@
+// Get username from localstorage, if no username then display "Guest"
 let myName = localStorage.getItem("name");
 if(myName) {
     document.getElementById("hello").innerText = myName;
 } else {
     document.getElementById("hello").innerText = 'Guest';
 }
+
+// Register global variables accessible by all functions in game
+let formattedQuestions = [];
+let index = 0;
+let correctAnswers = [];
+let wrongAnswers = [];
