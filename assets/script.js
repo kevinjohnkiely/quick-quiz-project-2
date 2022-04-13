8aed30918fc185bdc9df77d6c7d552b7264e6ff1
Kevin John Kiely <kevinjohnkiely1@hotmail.com>
2022-04-13 14:25:53 +0000

    Add fetch api code to request questions from API and console.log the result

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,6 @@
 // Get username from localstorage, if no username then display "Guest"
 let myName = localStorage.getItem("name");
-if(myName) {
+if (myName) {
     document.getElementById("hello").innerText = myName;
 } else {
     document.getElementById("hello").innerText = 'Guest';
@@ -19,24 +19,38 @@ document.addEventListener("DOMContentLoaded", function () {
     let buttons = document.getElementsByClassName("question-button");
 
     for (let button of buttons) {
-      button.addEventListener("click", function () {
-        let answer = this.getAttribute("data-type");
-        // check answers
-      });
+        button.addEventListener("click", function () {
+            let answer = this.getAttribute("data-type");
+            // check answers
+        });
     }
+
+    // Initialize game call
+    initGame()
 })
 
 /** This function initializes the game and populates the 10 questions from the Opentdb API */
-function initGame(){
+function initGame() {
+    // Pre populates DOM with loading message while waiting for data from API
+    document.getElementById("question-question").innerText = 'Loading Question 1...';
+
+    // Fetch 10 questions from API from general knowledge category
+    fetch("https://opentdb.com/api.php?amount=10&category=9")
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data)
+        }).catch(() => {
+            alert('Error getting data from OpenTDB API...')
+        });
 
 }
 
 /** This function displays the questions by loading the Opentdb API content into the DOM */
-function displayQuestion(){
+function displayQuestion() {
 
 }
 
 /** This function checks the selected answer against the correct answer from loaded array of questions */
-function checkAnswer(){
-    
+function checkAnswer() {
+
 }
