5f06937945a430ebc50d710fcbaa927a3823115e
Kevin John Kiely <kevinjohnkiely1@hotmail.com>
2022-04-13 13:41:30 +0000

    Add ID and data-type attributes to question buttons

--- assets/script.js
+++ assets/script.js
@@ -11,3 +11,17 @@ let formattedQuestions = [];
 let index = 0;
 let correctAnswers = [];
 let wrongAnswers = [];
+
+/** This function loads all event listeners after the DOM content has loaded,
+ *  and calls the initGame function to load the questions from the API.
+ */
+document.addEventListener("DOMContentLoaded", function () {
+    let buttons = document.getElementsByClassName("question-button");
+
+    for (let button of buttons) {
+      button.addEventListener("click", function () {
+        let answer = this.getAttribute("data-type");
+        // check answers
+      });
+    }
+})
